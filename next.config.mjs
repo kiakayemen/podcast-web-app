4f88f8ffe973c23f60899ca428cbdd189c7e3b08
kiakayemen <kiarashkarimian@outlook.com>
2025-01-05 22:31:09 +0330

    fixed the headers

--- next.config.mjs
+++ next.config.mjs
@@ -8,20 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; media-src 'self';",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
