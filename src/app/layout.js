1e74216a687aeb027f3f8dc9f159b8405e746a70
kiakayemen <kiarashkarimian@outlook.com>
2025-02-14 22:30:44 +0330

    test layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./storeProvider";
 import Header from "@/components/Header"
-import Player from "@/components/Player";
+import Player from "@/components/TestPlayer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Podcast App",
