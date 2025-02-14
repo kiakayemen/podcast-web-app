9bccdc13f3f38ba094b264945b3d8031ea81eafe
kiakayemen <kiarashkarimian@outlook.com>
2025-02-14 22:16:11 +0330

    adjusted the build script

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         >
         <Header />
           {children}
-          <Player />
+        <Player />
       </body>
         </StoreProvider>
     </html>
